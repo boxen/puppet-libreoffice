b943f06430f19e729fffdfab6a447bf3d8dbeff1
Toby Clemson <tobyclemson@gmail.com>
2014-06-15 19:40:40 -0400

    Upgrade to 4.1.6 and allow language pack version to be specified.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 #   class { 'libreoffice':
 #     version => '4.1.5',
 #   }
-class libreoffice($version='4.1.5') {
+class libreoffice($version='4.1.6') {
   package { "LibreOffice-${version}":
     provider => 'appdmg',
     source   => "http://download.documentfoundation.org/libreoffice/stable/${version}/mac/x86/LibreOffice_${version}_MacOS_x86.dmg",
--- spec/classes/languagepack_spec.rb
+++ spec/classes/languagepack_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe 'libreoffice::languagepack' do
+
+  version = '4.1.6'
+
   it do
     should contain_class('libreoffice::languagepack')
   end
@@ -8,7 +11,7 @@ describe 'libreoffice::languagepack' do
   context "default language" do
     it do
       should contain_package('LibreOffice LanguagePack').with({
-       :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.4/mac/x86/LibreOffice_4.1.4_MacOS_x86_langpack_de.dmg',
+       :source   => "http://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86/LibreOffice_#{version}_MacOS_x86_langpack_de.dmg",
        :provider => 'appdmg'
       })
     end
@@ -23,7 +26,22 @@ describe 'libreoffice::languagepack' do
 
     it do
       should contain_package('LibreOffice LanguagePack').with({
-        :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.4/mac/x86/LibreOffice_4.1.4_MacOS_x86_langpack_en-GB.dmg',
+        :source   => "http://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86/LibreOffice_#{version}_MacOS_x86_langpack_en-GB.dmg",
+        :provider => 'appdmg'
+      })
+    end
+  end
+
+  context "other version" do
+    let(:params) do
+      {
+        :version => '4.2.4'
+      }
+    end
+
+    it do
+      should contain_package('LibreOffice LanguagePack').with({
+        :source   => "http://download.documentfoundation.org/libreoffice/stable/4.2.4/mac/x86/LibreOffice_4.2.4_MacOS_x86_langpack_de.dmg",
         :provider => 'appdmg'
       })
     end
