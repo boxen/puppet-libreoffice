cea0eb70fbd9732eb0829006f5f03d263adee193
Adam Bowen <adamnbowen@gmail.com>
2014-08-12 10:34:46 -0500

    Update LibreOffice to version 4.3.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,9 +4,9 @@
 #
 #   include libreoffice
 #   class { 'libreoffice':
-#     version => '4.2.5',
+#     version => '4.3.0',
 #   }
-class libreoffice($version='4.2.5') {
+class libreoffice($version='4.3.0') {
   package { "LibreOffice-${version}":
     provider => 'appdmg',
     source   => "http://download.documentfoundation.org/libreoffice/stable/${version}/mac/x86_64/LibreOffice_${version}_MacOS_x86-64.dmg",
