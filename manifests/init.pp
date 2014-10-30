1f91ed9a60b942f3c22819b473b26e015b12eb2c
Romuald du Song <romuald.dusong@deveryware.com>
2014-10-30 09:40:01 +0100

    handle fix version

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,9 +6,9 @@
 #   class { 'libreoffice':
 #     version => '4.2.5',
 #   }
-class libreoffice($version='4.2.5') {
-  package { "LibreOffice-${version}":
+class libreoffice($version='4.2.5',$fix='') {
+  package { "LibreOffice-${version}${fix}":
     provider => 'appdmg',
-    source   => "http://download.documentfoundation.org/libreoffice/stable/${version}/mac/x86_64/LibreOffice_${version}_MacOS_x86-64.dmg",
+    source   => "http://download.documentfoundation.org/libreoffice/stable/${version}/mac/x86_64/LibreOffice_${version}${fix}_MacOS_x86-64.dmg",
   }
 }
