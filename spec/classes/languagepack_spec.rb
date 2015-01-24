2413deea402fd70ff016bf7f5fe452978a64f7bb
Marc Zottner <marc@zottner.com>
2015-01-24 13:14:25 +0100

    Update languagepack_spec.rb

--- spec/classes/languagepack_spec.rb
+++ spec/classes/languagepack_spec.rb
@@ -1,31 +1,18 @@
 require 'spec_helper'
 
 describe 'libreoffice::languagepack' do
+  version = '4.3.5'
+  locale = 'de'
+
   it do
     should contain_class('libreoffice::languagepack')
   end
 
-  context "default language" do
-    it do
-      should contain_package('LibreOffice LanguagePack').with({
-       :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.4/mac/x86/LibreOffice_4.1.4_MacOS_x86_langpack_de.dmg',
-       :provider => 'appdmg'
-      })
-    end
+  it do
+    should contain_package('LibreOffice LanguagePack').with({
+     :source   => "http://download.documentfoundation.org/libreoffice/stable/${version}/mac/x86/LibreOffice_${version}_MacOS_x86_langpack_${locale}.dmg",
+     :provider => "appdmg"
+    })
   end
 
-  context "other language" do
-    let(:params) do
-      {
-        :locale => 'en-GB'
-      }
-    end
-
-    it do
-      should contain_package('LibreOffice LanguagePack').with({
-        :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.4/mac/x86/LibreOffice_4.1.4_MacOS_x86_langpack_en-GB.dmg',
-        :provider => 'appdmg'
-      })
-    end
-  end
 end
